801d1cc8d629703425778b580110b312b96384e5
odunaikeiatwit <izzytkh@gmail.com>
2025-11-24 11:06:13 -0500

    Update parta.c

--- parta.c
+++ parta.c
@@ -1,8 +1,20 @@
-#include <stdbool.h>
-#include <stddef.h>
-
-// TODO: Include more header files (read the manpages to find out more!)
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 int main(int argc, const char* argv[]) {
+    if (argc < 2) {
+        printf("ERROR: No arguments\n");
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        for (int j = 0; argv[i][j] != '\0'; j++) {
+            putchar(toupper(argv[i][j]));
+        }
+        if (i < argc - 1) printf(",");
+    }
+
+    printf("\n");
     return 0;
 }
